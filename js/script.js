9d2b1cfc153d44e36170354b272348544d62e3f5
aletardio <ale.tardio99@gmail.com>
2023-11-30 16:46:14 +0100

    added cells red for bombs

--- js/script.js
+++ js/script.js
@@ -43,14 +43,31 @@ function createPlayGround(totalCells, cellsPerSide) {
 
         // Ottiene il riferimento al contenitore della griglia
         const grid = document.getElementById('grid');
+
+        // Dichiarazione della costante per il numero di bombe da visualizzare all'interno dell'array
+        const bombsToShow = 16;
+
+        const bombs = generateBombsList(bombsToShow);
+        console.log(bombs);
+
+        let points = 0; 
+
         // Definire un ciclo for per la realizzazione delle celle da 1 a 100
         for (let i=0; i<totalCells; i++) {
             // Creazione della singola cella
             let square = createCell(i+1, cellsPerSide);
             // Al click la cella cambierà di colore e la console emette il numero della cella
             square.addEventListener('click', function(){
-                this.classList.toggle('clicked');
-                console.log(this.innerText);
+
+                if(!bombs.includes(i)){
+                    this.classList.add('clicked');
+                    points++;
+
+                    document.getElementById('score').innerText = `Il tuo punteggio è pari a ${points}`;
+                }
+                else {
+                    this.classList.add('clicked-bombs');
+                }
             })
             // Appendo la cella alla griglia
             grid.appendChild(square);
@@ -88,9 +105,9 @@ function createCell (num, numPerSide) {
 function generateRandomNumbers (arrayBombs){
     let checkNumber = false;
 
+    let randomInt;
     while (!checkNumber) {
-        let randomInt = Math.floor(Math.random * 100 + 1);
-
+        randomInt = Math.floor(Math.random() * 100 + 1);
         if(!arrayBombs.includes(randomInt)){
             checkNumber = true;
         }
@@ -101,11 +118,12 @@ function generateRandomNumbers (arrayBombs){
 // Definizione della funzione che genera le singole bombe
 function generateBombsList (numberOfBombs){
     let bombs = [];
-    // Inserire all'interno drll'array 'bombs' un numero casuale
+    // Inserire all'interno dell'array 'bombs' un numero casuale
     for (let i=0 ; i < numberOfBombs ; i++){
         let bombNum = generateRandomNumbers(bombs);
         bombs.push(bombNum);
     }
+    return bombs;
 }
 
 
